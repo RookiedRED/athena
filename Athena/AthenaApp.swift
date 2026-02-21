5f62ab01419d417efe3287c3de86241e01b7532f
Oracle Lin <hongyulin715@gmail.com>
2026-02-20 21:10:36 -0600

    Initial Commit

---
+++ Athena/AthenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaApp.swift
+//  Athena
+//
+//  Created by Lin, Hung Yu on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct AthenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
